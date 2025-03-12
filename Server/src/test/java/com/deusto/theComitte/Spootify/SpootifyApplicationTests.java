dcd759cb8a70f1fd102c6e3293fe80a7bd9f2d9f
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-03-12 13:15:38 +0100

    Project setup

---
+++ Server/src/test/java/com/deusto/theComitte/Spootify/SpootifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.theComitte.Spootify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpootifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
