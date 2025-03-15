e12152c7183c10a0c788979324bf3d1ae67cbc01
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-03-15 20:24:44 +0100

    Empiezo a entender vue

---
+++ Spootify-client/src/components/Prueba.vue
@@ -0,0 +1,12 @@
+<script setup>
+    let songs = [{ title: 'Song 1', artist: 'Artist 1' }, { title: 'Song 2', artist: 'Artist 2' }]
+</script>
+
+
+<template>
+    <div class="song" v-for="song in songs">
+        <h3>{{ song.title }}</h3>
+        <p>{{ song.artist }}</p>
+    </div>
+    
+</template>
