3931a6f35ea70d27389fc23da8fbfac07faa5e5f
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-03-15 21:06:12 +0100

    La otra parte

--- Spootify-client/src/components/Prueba.vue
+++ Spootify-client/src/components/Prueba.vue
@@ -1,12 +1,22 @@
 <script setup>
-    let songs = [{ title: 'Song 1', artist: 'Artist 1' }, { title: 'Song 2', artist: 'Artist 2' }]
+    import axios from "axios";
+    import {onMounted} from "vue";
+    import {ref} from "vue";
+
+    let songs = ref([]) //ref añade reactividad
+    onMounted(() => {
+        axios.get("http://localhost:8081/songs").then((response) => {
+            songs.value = response.data;
+            console.log(songs.value);
+        });
+    });
+
 </script>
 
 
 <template>
-    <div class="song" v-for="song in songs">
-        <h3>{{ song.title }}</h3>
-        <p>{{ song.artist }}</p>
+    <div class="song" v-for="song in songs" :key="song.title"> <!-- Key para reaccionar bien a los cambios-->
+        <p>{{ song }}</p>
     </div>
     
 </template>
