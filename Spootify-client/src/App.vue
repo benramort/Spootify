a7377f01cb5c7efd58368871f0f267fddb0e9922
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-03-16 11:47:37 +0100

    Merge de la otra rama

---
+++ Spootify-client/src/App.vue
@@ -0,0 +1,23 @@
+<!-- Para lanzar: 
+ (npm install)
+ npm run dev-->
+
+<script setup> //Para no poner export default
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Prueba from './components/Prueba.vue'
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    <Prueba/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Spootify-client/src/components/Prueba.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import axios from "axios";
+    import {onMounted} from "vue";
+    import {ref} from "vue";
+
+    let songs = ref([]) //ref añade reactividad
+    onMounted(() => {
+        axios.get("http://localhost:8081/songs").then((response) => {
+            songs.value = response.data;
+            console.log(songs.value);
+        });
+    });
+
+</script>
+
+
+<template>
+    <div class="song" v-for="song in songs" :key="song.title"> <!-- Key para reaccionar bien a los cambios-->
+        <p>{{ song }}</p>
+    </div>
+    
+</template>
