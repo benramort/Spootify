f23a9f5c74f870fa5acd3b23fee6fe10cbe9251c
AdeiLB <145749081+AdeiLB@users.noreply.github.com>
2025-03-25 14:53:15 +0100

    Detalle artista vue

---
+++ Spootify-client/src/views/ArtistDetail.vue
@@ -0,0 +1,100 @@
+<script setup>
+    import { onMounted, ref, inject } from 'vue';
+    import Prueba from '../components/Songs.vue';
+    import Albums from "../components/Albums.vue";
+    import { useRoute } from 'vue-router';
+    import axios from 'axios';
+
+    let globalState = inject('globalState');
+
+    const route = useRoute();
+
+    const name = ref("");
+
+    // onMounted(() => {
+    //     let path = "http://localhost:8081/artists/" + route.params.id;
+    //     axios.get(path).then((response) => {
+    //         console.log(response.data);
+    //         name.value = response.data.name;
+    //         console.log(name.value);
+    //     }).catch((error) => {
+    //         console.log(error);
+    //     });
+    // });
+</script>
+
+<template>
+    <p>ssssss</p>
+    <div class="template">
+        <p>Detalle</p>
+        <h1>{{ name }}:</h1>
+        <div class="columns">
+            <div class="column">
+                <Albums />
+            </div>
+            <div class="column">
+                <Prueba />
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped>
+
+
+
+    .columns {
+        display: flex;
+        justify-content: space-between;
+        border: 5px solid blue;
+        height: 100%;
+    }
+
+    .column {
+        border: 2px solid red;
+        width: 50%;
+        height: 68vh;
+    }
+
+    button {
+        width: 49%;
+        height: 100%;
+        margin: 10px auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: #fff;
+        background: rgb(30, 215, 96);
+        font-size: 1em;
+        font-weight: bold;
+        outline: none;
+        border: none;
+        border-radius: 1em;
+        transition: .2s ease-in;
+        cursor: pointer;
+    }
+
+    button:hover{
+	    background: rgb(22, 164, 72);
+    }
+
+    .botonera {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 50%;
+        border: 1px solid aquamarine;
+    }
+
+    .inside-button {
+        margin: 0.2em;
+    }
+
+    i {
+        font-size: 4em;
+    }
+
+</style>
