153125dbffaad5ca5755919db2ac312d88745de1
GaizkaCarrasco <gaizka.carrasco@opendeusto.es>
2025-03-30 16:10:11 +0200

    no se pueden crear playlist con el nombre vacío

--- Spootify-client/src/components/CreatePlayList.vue
+++ Spootify-client/src/components/CreatePlayList.vue
@@ -4,12 +4,16 @@
             <div id="titulo">
                 <p id="crearPlayList">CREAR PLAYLIST</p>
             </div>
+            <!-- Mensaje de error entre el título y el cuadro de texto -->
+            <div v-if="errorMessage" id="errorMessage">
+                <p>{{ errorMessage }}</p>
+            </div>
             <div id="campos">
                 <div id="campoNombre">
                     <input id="inputNombre" type="text" placeholder="Name" v-model="playListName" />
                 </div>
                 <div id="button">
-                    <button @click="$emit('close'); console.log(playListName); createPlayList();" id="okButton">✔</button>
+                    <button @click="handleCreatePlayList" id="okButton">✔</button>
                 </div>
             </div>
         </div>
@@ -19,12 +23,20 @@
 <style scoped>
 #contFormulario {
     width: 300px;
-    height: 120px;
+    height: 150px; /* Ajusta la altura para incluir el mensaje de error */
     border: 2px solid rgb(34, 34, 34);
     margin: 0 auto;
     background-color: rgb(34, 34, 34);
     box-shadow: 5px 10px 20px black;
     border-radius: 10px;
+}
+
+#errorMessage {
+    color: red;
+    font-size: 0.9em;
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 10px; /* Espacio entre el mensaje y el cuadro de texto */
 }
 
 #titulo {
@@ -43,7 +55,7 @@
     display: flex;
     align-items: center;
     margin: 0 auto;
-    margin-top: 20px;
+    margin-top: 10px;
     margin-bottom: 20px;
     display: inline-block;
 }
@@ -109,16 +121,30 @@ import axios from 'axios';
 const globalState = inject("globalState");
 const showModalPlayList = ref(false);
 const playListName = ref("");
+const errorMessage = ref(""); // Variable para almacenar el mensaje de error
+
+function handleCreatePlayList() {
+    if (!playListName.value.trim()) {
+        errorMessage.value = "El nombre de la playlist no puede estar vacío.";
+        return;
+    }
+
+    createPlayList();
+}
 
 function createPlayList() {
     let path = "http://localhost:8081/playlists";
     path += "?token=" + globalState.token.value;
-    axios.post(path, { "name": playListName.value }).then((response) => {
-        console.log(response);
-        console.log("Playlist created");
-        location.reload();
-    }).catch((error) => {
-        console.log(error);
-    });
+
+    axios.post(path, { "name": playListName.value })
+        .then((response) => {
+            console.log(response);
+            console.log("Playlist created");
+            location.reload();
+        })
+        .catch((error) => {
+            console.log(error);
+            errorMessage.value = "Error al crear la playlist. Inténtalo de nuevo.";
+        });
 }
 </script>
