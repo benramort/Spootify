1aae6f64a48505b39cecdf54b0919ef13d2a881b
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-04-07 18:05:15 +0200

    Plataforma para pruebas de integración

---
+++ Server/src/test/java/com/deusto/theComitte/Spootify/integration/prueba.java
@@ -0,0 +1,14 @@
+package com.deusto.theComitte.Spootify.integration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class prueba {
+
+    // @Test
+    // public void pruebita() {
+    //     assertTrue(false);
+    // }
+    
+}
