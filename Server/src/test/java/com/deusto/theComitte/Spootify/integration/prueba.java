49a295071c3a47625a74c9251bb04d0b05d7fc04
Beñat Ramirez <benat.ramirez@opendeusto.es>
2025-04-26 12:51:34 +0100

    Creando pruebas de integracion

--- Server/src/test/java/com/deusto/theComitte/Spootify/integration/prueba.java
+++ Server/src/test/java/com/deusto/theComitte/Spootify/integration/prueba.java
@@ -1,5 +1,6 @@
 package com.deusto.theComitte.Spootify.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URI;
@@ -16,7 +17,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.StreamingHttpOutputMessage.Body;
 
+import com.deusto.theComitte.Spootify.DTO.CreateUserDTO;
+import com.deusto.theComitte.Spootify.DTO.LoginDTO;
 import com.deusto.theComitte.Spootify.DTO.SongDTO;
+import com.deusto.theComitte.Spootify.DTO.UserDTO;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -38,23 +42,66 @@ public class prueba {
     }
 
     @Test
-    public void integrationTest1() {
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8081/songs"))
-            .GET()
+    public void integrationTest1() { //Crear artista --> Iniciar sesión --> Crear álbum --> Crear canción
+        try {
+            String NAME = "artist1";
+            String EMAIL = "artist1@artist1";
+            String PASSWORD = "artist1";
+
+            // Crear artista
+
+            CreateUserDTO user = new CreateUserDTO(NAME, EMAIL, PASSWORD);
+        
+            HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8081/artists"))
+            .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(user)))
+            .header("Content-Type", "application/json")
             .build();
 
-        try {
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            System.out.println(response.body());
-            
-            List<SongDTO> responseObjects = objectMapper.readValue(response.body(), new TypeReference<List<SongDTO>>() {});
-            System.out.println(responseObjects.get(0).getId());
+            assertEquals(200, response.statusCode());
+
+            // Iniciar sesión
+
+            LoginDTO login = new LoginDTO(EMAIL, PASSWORD);
+            request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8081/artists/login"))
+            .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(login)))
+            .header("Content-Type", "application/json")
+            .build();
+
+            response = client.send(request, BodyHandlers.ofString());
+            assertEquals(200, response.statusCode());
+
+            long TOKEN = objectMapper.readTree(response.body()).get("token").asLong();
+            System.out.println("Token: " + TOKEN);
+
+            // Crear álbum
+            String ALBUM_NAME = "album1";
+
+            request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8081/albums?"+"token="+TOKEN+"&name="+ALBUM_NAME))
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .header("Content-Type", "multipart/form-data")
+            .build();
+
+            response = client.send(request, BodyHandlers.ofString());
+            assertEquals(200, response.statusCode());
+
+            System.out.println("Body: " + response.body());
+
+
+
+
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         
     }
+
+    //Crear usuario --> Iniciar sesión --> Crear playlist --> Añadir canción a playlist
+
+    //Crear usuario --> Iniciar sesión --> Dar like a canción --> Seguir artista
     
 }
