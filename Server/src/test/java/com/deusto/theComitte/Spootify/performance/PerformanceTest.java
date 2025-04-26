31ed88ecbc90e1400cdae9d2462b319ffa3a28ed
Iker Veiga <veiga.iker@opendeusto.es>
2025-04-26 10:10:16 +0200

    Seguir intentando tests de rendimiento

--- Server/src/test/java/com/deusto/theComitte/Spootify/performance/PerformanceTest.java
+++ Server/src/test/java/com/deusto/theComitte/Spootify/performance/PerformanceTest.java
@@ -9,6 +9,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
@@ -23,27 +25,42 @@ public class PerformanceTest {
     @Rule 
 	public JUnitPerfRule perfTestRule = new JUnitPerfRule(new HtmlReportGenerator("target/junitperf/report.html"));
 
+    @BeforeEach
+    public void cleanDB() throws Exception {
+        //Clean database
+        HttpRequest deleteUserRequest = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8081/users"))
+                .header("Content-Type", "application/json")
+                .DELETE()
+                .build();
+        HttpResponse<String> deleteUserResponse = HttpClient.newHttpClient().send(deleteUserRequest, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, deleteUserResponse.statusCode()); // Assuming 200 OK for user deletion
+        
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        //Create the user before attempting login
+        HttpRequest createUserRequest = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8081/users"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"name\":\"user2\", \"email\":\"user2@user2\", \"password\":\"password\"}"))
+                .build();
+        HttpResponse<String> createUserResponse = HttpClient.newHttpClient().send(createUserRequest, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, createUserResponse.statusCode()); // Assuming 201 Created for user registration
+    }
+
     @Test
     @JUnitPerfTest(threads = 10, durationMs = 5000)
     @JUnitPerfTestRequirement(meanLatency = 100)
     public void testLogin() throws Exception {
         System.out.println("Running testLogin");
 
-        // Create the user before attempting login
-        HttpRequest createUserRequest = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:8081/users"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString("{\"name\":\"user1\", \"email\":\"user1@user1\", \"password\":\"password\"}"))
-                .build();
-
-        HttpResponse<String> createUserResponse = HttpClient.newHttpClient().send(createUserRequest, HttpResponse.BodyHandlers.ofString());
-        assertEquals(200, createUserResponse.statusCode()); // Assuming 201 Created for user registration
-
         // Perform login
         HttpRequest loginRequest = HttpRequest.newBuilder()
                 .uri(new URI("http://localhost:8081/login"))
                 .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString("{\"name\":\"user1\", \"email\":\"user1@user1\", \"password\":\"password\"}"))
+                .POST(HttpRequest.BodyPublishers.ofString("{\"name\":\"user2\", \"email\":\"user2@user2\", \"password\":\"password\"}"))
                 .build();
 
         HttpResponse<String> loginResponse = HttpClient.newHttpClient().send(loginRequest, HttpResponse.BodyHandlers.ofString());
