dae8abdbb13a44474919c7d0d80b9b16bcb663a7
AdeiLB <145749081+AdeiLB@users.noreply.github.com>
2025-05-06 15:26:32 +0200

    ultima mejora de búsqueda

--- Server/src/main/java/com/deusto/theComitte/Spootify/DAO/ArtistRepository.java
+++ Server/src/main/java/com/deusto/theComitte/Spootify/DAO/ArtistRepository.java
@@ -13,6 +13,6 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
     Artist findById(long id);
     Artist findByEmail(String email);
 
-    @Query("SELECT a FROM Artist a WHERE LOWER(a.name) LIKE LOWER(CONCAT(:name, '%'))")
+    @Query("SELECT a FROM Artist a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%',:name, '%'))")
     List<Artist> findByName(String name);
 }
